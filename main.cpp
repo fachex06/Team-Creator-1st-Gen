f8043edd12fedac1040892818c50f722706fc103
Difel_w <lomelimiguel00@gmail.com>
2025-11-17 23:26:22 -0600

    I forgor to update the main file 💀
    sorry.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "Punteros.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Punteros pokedex;
+    pokedex.mostrarMenu();
     return 0;
 }
